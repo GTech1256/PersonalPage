7942dbbe4e5cc0cf3c8772303cfc70dfa3856d6f
Roman <pomah156@gmail.com>
2018-08-05 22:21:50 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  var del = document.getElementById('del');
+  del.parentNode.removeChild(del)
+}
+
+window.onkeydown = function(e) {
+  document.location.href='https://vk.com/greentech1256';
+};
